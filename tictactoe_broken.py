e2096357c7208d3bd8c049d6d7531f4266157b3a
Txkxn <nathansandep@gmail.com>
2026-03-06 20:44:22 +0400

    Fixed tictactoe_broken.py

--- tictactoe_broken.py
+++ tictactoe_broken.py
@@ -44,8 +44,8 @@ def check_winner(game):
         winner = check_row_winner(game_slice)
         if winner != 0:
             return winner
-
-    return winner
+    
+    return 0 # Correctly return 0 if no winner exists (main loop condition)
 
 def start_game():
     return [[0, 0, 0] for x in range(3)]
@@ -67,7 +67,7 @@ def add_piece(game, player, row, column):
     row: 0-index row
     column: 0-index column
     """
-    game[row][column+1] = player
+    game[row][column] = player # Removed the column addition (shifts everything up...)
     return game
 
 def check_space_empty(game, row, column):
@@ -77,7 +77,7 @@ def convert_input_to_coordinate(user_input):
     return user_input - 1
 
 def switch_player(player):
-    if player = 1:
+    if player == 1: # Now uses comparison operator
         return 2
     else:
         return 1
@@ -98,12 +98,12 @@ if __name__ == '__main__':
     while winner == 0 and moves_exist(game):
         print("Currently player: " + str(player))
         available = False
-        while not available
+        while not available: # Missing semicolon
             row = convert_input_to_coordinate(int(input("Which row? (start with 1) ")))
             column = convert_input_to_coordinate(int(input("Which column? (start with 1) ")))
-            available = check_space_empty(game, row)
+            available = check_space_empty(game, row, column) # Missing Parameter for function
         game = add_piece(game, player, row, column)
         display_game(game)
         player = switch_player(player)
-#        winner = check_winner(game)
+        winner = check_winner(game) # uncommented this line
     display_winner(winner)
